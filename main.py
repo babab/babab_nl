2727d540ae3b23f4ce0101545221e8da02e0d84e
Benjamin Althues <benjamin@babab.nl>
2013-07-29 22:55:01 +0200

    Add rot13 tool

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python2
 import web
+from web import form
 
 render = web.template.render('tpl/', base='base')
 urls = (
     '/',            'index',
     '/dispass/',    'dispass',
+    '/rot13/',      'rot13',
     '/springwhiz/', 'springwhiz',
 )
+app = web.application(urls, globals())
+
+rot13_form = form.Form(form.Textarea('text'))
 
 
 class index:
@@ -23,6 +28,23 @@ class springwhiz:
     def GET(self):
         return render.springwhiz()
 
+
+class rot13:
+    def GET(self):
+        form = rot13_form()
+        return render.rot13(form)
+
+    def POST(self):
+        form = rot13_form()
+        if form.validates():
+            return render.rot13(form)
+        else:
+            return 'Invalid entry'
+
+
+class strgen:
+    def GET(self):
+        return render.strgen()
+
 if __name__ == '__main__':
-    app = web.application(urls, globals())
     app.run()
