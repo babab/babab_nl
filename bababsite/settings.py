ef0c783f5f3b9dc4a037813c4d01ce48fe9f0da2
Benjamin Althues <benjamin@babab.nl>
2015-07-04 13:55:24 +0200

    Use plain old HTML for templates

--- bababsite/settings.py
+++ bababsite/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'bababsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'tpl', 'html')],
+        'DIRS': [os.path.join(BASE_DIR, 'tpl')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
